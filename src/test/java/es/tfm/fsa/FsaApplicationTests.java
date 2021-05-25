bbbb711990602fa2caef15561f7b149c320cead9
Jordy Arrese <jordyarresehuaman@gmail.com>
2021-05-26 00:20:45 +0200

    initial commit

---
+++ src/test/java/es/tfm/fsa/FsaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.tfm.fsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
